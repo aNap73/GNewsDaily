3593cae7bb3de23420ec2ab386312c76ac8a9973
aNap73 <napolitano.anthony@yahoo.com>
2018-06-11 16:11:17 -0400

    began depreciating hamburger; worked on style and bootstrap; beginning save implementation

--- controllers/article_controller.js
+++ controllers/article_controller.js
@@ -15,11 +15,12 @@ var express = require("express");
 var router = express.Router();
 var cheerio = require("cheerio");
 var request = require("request");
+var HighLight = 'style="font-weight:600; color:#007bff"';
+var LowLight = 'style="font-weight:100; color:#5e6a77"';
+//mod = require('../models/burgers');
+var mod2= require('../models_2/')
+var arrOut = [];
 
-mod = require('../models/burgers');
-mod2= require('../models_2/')
-
-//function GetFreshArticles () {}
 class Article {
   constructor(title, summary, link, imglink, author){
     this.Title = title;
@@ -30,78 +31,106 @@ class Article {
   }
 }
 const getFreshArticles = (res) => {
-let arrOut = [];
+console.log('start');
+arrOut.length=0;
+console.log('requestive massively');
 request("http://massivelyop.com/", function(error, response, html) {
-var $ = cheerio.load(html);
+ let $ = cheerio.load(html);
+ let iEnd = 8;
 $("article.post").each(function(i,element){
-  let link = $(element).children("h2").children().attr("href");
+  if(i<iEnd){
+  let link = 'http://massivelyop.com/' + $(element).children("h2").children().attr("href");
   let title = $(element).children("h2").text();  
-  let summary = $(element).children(".entry").children("p").toString().replace(/<\/?(?!p)\w*\b[^>]*>/g, "").replace(/&#x[^\s][^\s][^\s][^\s];/g,"")
+  let summary = $(element).children(".entry").children("p").toString().replace(/<\/?(?!p)\w*\b[^>]*>/g, "").replace(/&#x[^\s][^\s][^\s][^\s];/g,"").replace(/<p> Read more<\/p>|<p> Comment<\/p>/g,"");
   let linkimg = $(element).children("a").children("img").attr("src");
   let out = new Article(title, summary, link, linkimg, '');
-  arrOut.push(out);
+  if (summary.length > 100){
+    arrOut.push(out);
+  } else {if(iEnd<25){iEnd++}};
+ 
+  }
 });
+console.log('done massively');
+console.log('requestive mmorpg');
 request("https://www.mmorpg.com/", function(error, response, html) {
-    var $ = cheerio.load(html);
-    
+    let $ = cheerio.load(html);
+    let iEnd = 8;
     $(".newsitem").each(function(i,element){
-      let link = $(element).children("h3").children().attr("href");
+      if(i<iEnd){
+      let link = 'https://www.mmorpg.com/' + $(element).children("h3").children().attr("href");
       let title = $(element).children("h3").text();  
       let summary = $(element).children(".news_newspost").children("p").toString().replace(/<\/?(?!p)\w*\b[^>]*>/g, "").replace(/&#x[^\s][^\s][^\s][^\s];/g,"")
       
       let linkimg = $(element).children(".news_newspost").children("a").children("img").attr("src");
       let out = new Article(title, summary, link, linkimg, '');
-    
-      arrOut.push(out);
-    
+      if (summary.length > 100){
+        arrOut.push(out);
+      } else {if(iEnd<25){iEnd++}};
+     
+      }
      });
-     console.log('Articles---->', arrOut);
+     //console.log('Articles---->', arrOut);
+     console.log('done mmorpg');   
      var artObject = {
-      articles: arrOut
-    };   
-    res.render("index2", artObject);
+      articles: arrOut,      
+      styleHomeLink: HighLight,
+      styleSavedLink: LowLight,
+      styleAboutLink: LowLight,
+      btnSave: "style='color:white;'"}; 
+    console.log('render');
+    return (res.render("index", artObject));
     })
 
 }) 
 
-  return arrOut;
-}
-router.get("*", function(req, res) {
-  
-  mod.getallburgers(function(data) {
 
-    getFreshArticles(res);
+}
+router.get("*", function(req, res) {  
+  
+    if(arrOut.length<1){
+      getFreshArticles(res);
+    }else{
+      var artObject = {
+        articles: arrOut,
+        styleHomeLink: HighLight,
+        styleSavedLink: LowLight,
+        styleAboutLink: LowLight,
+        btnSave: "style='color:white;'"
+      }; 
+      return (res.render("index", artObject));
+    }
     
-  });
+    
+  
 
   
 });
 
 router.post("/api/burgers/", function(req,res){
-  mod.insertaburger(function(result) {
-    res.json({ id: result.insertId });}, {BURGER_NAME: req.body.BURGER_NAME, DEVOURED: req.body.DEVOURED});
+  //mod.insertaburger(function(result) {
+  //  res.json({ id: result.insertId });}, {BURGER_NAME: req.body.BURGER_NAME, DEVOURED: req.body.DEVOURED});
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-   mod.updateaburger(function(result) {
-    if (result.changedRows === 0) {
-     
-      return res.status(403).end();
-    }
-    res.status(200).end();}, {DEVOURED: req.body.DEVOURED}, req.params.id);
+  // mod.updateaburger(function(result) {
+  //  if (result.changedRows === 0) {
+  //   
+  //    return res.status(403).end();
+  //  }
+  //  res.status(200).end();}, {DEVOURED: req.body.DEVOURED}, req.params.id);
 });
 router.delete("/api/burgers/", function(req, res) {
   
-    mod.deleteburgers(function(result) {
-      res.status(200).end();},  {DEVOURED: 1});
+    //mod.deleteburgers(function(result) {
+    //  res.status(200).end();},  {DEVOURED: 1});
   
   
 });
 router.delete("/api/burgers/:id", function(req, res) {
-  if(req.params.id){
-    mod.deleteburgers(function(result) {
-      res.status(200).end();},  {id: req.params.id});
-  }
+  //if(req.params.id){
+  //  mod.deleteburgers(function(result) {
+ //     res.status(200).end();},  {id: req.params.id});
+ // }
   
 });
 
